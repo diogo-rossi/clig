f0810b5f07b7f2b574feaafe6ebcc710439b7717
Diogo Rossi <rossi.diogo@gmail.com>
2024-11-26 18:17:28 -0300

    🚧 wip: new classes and functions

--- src/clig/clig.py
+++ src/clig/clig.py
@@ -47,20 +47,242 @@ Parameters
     {{parameter_description}}
 """
 
+SUBPARSERS_DEST = "subparser_name"
+
+
+@dataclass
+class Command:
+    func: Callable[..., Any] | None = None
+    prog: str | None = None
+    usage: str | None = None
+    description: str | None = None
+    epilog: str | None = None
+    parents: Sequence[ArgumentParser] = field(default_factory=list)
+    formatter_class: type[HelpFormatter] = HelpFormatter
+    prefix_chars: str = "-"
+    fromfile_prefix_chars: str | None = None
+    argument_default: Any = None
+    conflict_handler: str = "error"
+    add_help: bool = True
+    allow_abbrev: bool = True
+    exit_on_error: bool = True
+    _: KW_ONLY
+    subcommands_title: str = "subcommands"
+    subcommands_description: str | None = None
+    subcommands_prog: str | None = None
+    subcommands_required: bool = False
+    subcommands_help: str | None = None
+    subcommands_metavar: str | None = None
+    docstring_template: str | None = None
+
+    def __post_init__(self):
+        self.parameters: Mapping[str, Parameter] = {}
+        if self.func:
+            self.parameters = inspect.signature(self.func).parameters
+        self.argument_data: list[ArgumentData] = self.get_argument_data()
+        self.parent: Command | None = None
+        self.sub_commands: list[Command] = []
+
+    @overload
+    def command[**P, T](self, func: Callable[P, T]) -> Callable[P, T]: ...
+
+    @overload
+    def command[**P, T](self, **kwargs) -> Callable[[Callable[P, T]], Callable[P, T]]: ...
+
+    def command[**P, T](
+        self,
+        func: Callable[P, T] | None = None,
+        **kwargs,
+    ) -> Callable[P, T] | Callable[[Callable[P, T]], Callable[P, T]]:  # fmt: skip
+        if func is not None:
+            self.new_command(func)
+            return func
+
+        def wrap(func):
+            self.new_command(func, **kwargs)
+            return func
+
+        return wrap
+
+    def add_command(self, func: Callable[..., Any], *args, **kwargs) -> Self:
+        self.new_command(func, *args, **kwargs)
+        return self
+
+    def new_command(self, func: Callable[..., Any], *args, **kwargs) -> Command:
+        count = -1
+        parent_parser = self.parent
+        while parent_parser:
+            count += 1
+            parent_parser = parent_parser.parent
+        self.subparsers_dest = f"{SUBPARSERS_DEST}{'_' if count >= 0 else ""}{count if count >= 0 else ''}"
+        cmd: Command = Command(func, *args, **kwargs)
+        self.sub_commands.append(cmd)
+        cmd.parent = self
+        return cmd
+
+    def get_argument_data(self) -> list[ArgumentData]:
+        argument_data: list[ArgumentData] = []
+
+        docstring_data: DocstringData | None = self.get_docstring_data()
+
+        helps: dict[str, str] = {}
+        if docstring_data:
+            helps: dict[str, str] = docstring_data.helps
+            self.description = docstring_data.description
+            self.epilog = docstring_data.epilog
+
+        for par in self.parameters:
+            data = get_metadata_from_parameter(self.parameters[par])
+            if not data.kwargs.get("help") and helps.get(data.name):
+                data.kwargs["help"] = helps[data.name]
+            argument_data.append(data)
+        return argument_data
+
+    def get_docstring_data(self) -> DocstringData | None:
+        template = self.docstring_template
+        parameter_number = len(self.parameters)
+        docstring = normalize_docstring(self.func.__doc__)
+        # escape for regex match, but not "{" and "}"
+        template = re.escape(normalize_docstring(template) + "\n").replace(r"\{", "{").replace(r"\}", "}")
+        place_holders: dict[str, list[int]] = {
+            "description": [],
+            "epilog": [],
+            "parameter_name": [],
+            "parameter_type": [],
+            "parameter_description": [],
+        }
+        detected_place_holders: list[str] = re.findall(r"{{.*?}}", template)
+        order_counter = 0
+        for word in detected_place_holders:
+            word = word.removeprefix("{{").removesuffix("}}")
+            if word in place_holders:
+                place_holders[word].append(order_counter)
+                order_counter += 1
+        parameter_section_init_index: int = 0
+        for i, line in enumerate(template.splitlines()):
+            if any([f"{{{{{key}}}}}" in line for key in place_holders if key.startswith("parameter")]):
+                parameter_section_init_index = i
+                break
+        parameter_section_length = sum(
+            [template.count(f"{{{{{key}}}}}") for key in place_holders if key.startswith("parameter")]
+        )
+        parameter_section = "\n".join(template.splitlines()[parameter_section_init_index:])
+        for _ in range(parameter_number - 1):
+            template += f"{parameter_section}\n"
+        for place_holder in place_holders:
+            template = template.replace(f"{{{{{place_holder}}}}}", "(.*?)")
+        template += "(.*)"
+        match = re.match(template, docstring, re.DOTALL)
+        if match:
+            matches: tuple[str, ...] = match.groups()
+            docstring_data = DocstringData(
+                matches[place_holders["description"][0]], matches[place_holders["epilog"][0]]
+            )
+            for i in range(parameter_number):
+                docstring_data.helps[
+                    matches[place_holders["parameter_name"][0] + parameter_section_length * i]
+                ] = matches[place_holders["parameter_description"][0] + parameter_section_length * i].strip()
+            return docstring_data
+        return None
+
+    def inferarg(self, argdata: ArgumentData) -> tuple[tuple[str, ...], KeywordArguments]:
+        """"""
+        ...
+
+    def add_parsers(self) -> None:
+        """"""
+        ...
+
+    def run(self, args: Sequence[str] | None = None) -> None:
+        if not self.is_main_command:
+            return
+
+    @property
+    def is_main_command(self) -> bool:
+        return self.parent is None
+
+
+class Application:
+    main: Callable[..., Any] | None = None
+    prog: str = ""
+    prefix_chars: str = "-"
+
+    def __post_init__(self):
+        self.parser = ArgumentParser()
+
+    def command[**P, T](self, func: Callable[P, T]) -> Callable[P, T]:
+        return func
+
+    def add_command(self, func: Callable[..., Any]) -> Self:
+        return self
+
+    def new_command(self, func: Callable[..., Any]) -> Command:
+        return Command()
+
+
+class ArgumentMetaDataDictionary(TypedDict, total=False):
+    action: str
+    nargs: int | str | None
+    const: Any
+    choices: Iterable | None
+    required: bool | None
+    help: str | None
+    metavar: str | tuple[str, ...] | None
+    version: str | None
+
+
+class KeywordArguments(ArgumentMetaDataDictionary, total=False):
+    default: Any
+    type: type
+
+
+def prepare_kwargs(**kwargs: Unpack[KeywordArguments]) -> KeywordArguments:
+    return kwargs
+
+
+@dataclass
+class ArgumentGroup:
+    title: str | None = None
+
+
+@dataclass
+class MutuallyExclusiveGroup:
+    required: bool = False
+
+
+@dataclass
+class ArgumentMetaData:
+    flags: list[str]
+    make_flag: bool | None = None
+    argument_group: ArgumentGroup | None = None
+    mutually_exclusive_group: MutuallyExclusiveGroup | None = None
+    dictionary: KeywordArguments = field(default_factory=KeywordArguments)
+
+
+from inspect import _ParameterKind
+
+a: Literal[_ParameterKind.POSITIONAL_OR_KEYWORD] = Parameter.POSITIONAL_OR_KEYWORD
+
+
+@dataclass
+class ArgumentData:
+    name: str
+    type: Callable[[str], Any] | FileType | str | None = None
+    kind: Literal[""] | None = None
+    default: Any = None
+    flags: list[str] | None = field(default_factory=list)
+    kwargs: KeywordArguments | dict = field(default_factory=dict)
+    make_flag: bool | None = None
+    argument_group: ArgumentGroup | None = None
+    mutually_exclusive_group: MutuallyExclusiveGroup | None = None
+    parser: Any = None
+
 
 @dataclass
 class DocstringData:
     description: str
     epilog: str
     helps: dict[str, str] = field(default_factory=dict)
-
-
-@dataclass
-class ArgumentData:
-    name: str
-    kind: Callable = str
-    default: Any = None
-    help: str | None = None
 
 
 def count_leading_spaces(string: str):
@@ -83,52 +305,78 @@ def normalize_docstring(docstring: str | None) -> str:
     return "\n".join(lines)
 
 
-def get_docstring_data(
-    parameter_number: int, docstring: str | None, template: str = NUMPY_DOCSTRING
-) -> DocstringData | None:
-    docstring = normalize_docstring(docstring)
-    template = (  # escape for regex match, but not "{" and "}"
-        re.escape(normalize_docstring(template) + "\n").replace(r"\{", "{").replace(r"\}", "}")
+def data(
+    *flags: str,
+    make_flag: bool | None = None,
+    group: ArgumentGroup | None = None,
+    mutually_exclusive_group: MutuallyExclusiveGroup | None = None,
+    **kwargs: Unpack[KeywordArguments],
+) -> ArgumentMetaData:
+    return ArgumentMetaData(
+        flags=list(flags),
+        make_flag=make_flag,
+        argument_group=group,
+        mutually_exclusive_group=mutually_exclusive_group,
+        dictionary=kwargs,
     )
-    place_holders: dict[str, list[int]] = {
-        "description": [],
-        "epilog": [],
-        "parameter_name": [],
-        "parameter_type": [],
-        "parameter_description": [],
-    }
-    detected_place_holders: list[str] = re.findall(r"{{.*?}}", template)
-    order_counter = 0
-    for word in detected_place_holders:
-        word = word.removeprefix("{{").removesuffix("}}")
-        if word in place_holders:
-            place_holders[word].append(order_counter)
-            order_counter += 1
-    parameter_section_init_index: int = 0
-    for i, line in enumerate(template.splitlines()):
-        if any([f"{{{{{key}}}}}" in line for key in place_holders if key.startswith("parameter")]):
-            parameter_section_init_index = i
-            break
-    parameter_section_length = sum(
-        [template.count(f"{{{{{key}}}}}") for key in place_holders if key.startswith("parameter")]
+
+
+def arg(
+    *flags: str,
+    make_flag: bool | None = None,
+    group: ArgumentGroup | None = None,
+    mutually_exclusive_group: MutuallyExclusiveGroup | None = None,
+    subparser: Field[Any] | None = None,
+    **kwargs: Unpack[KeywordArguments],
+) -> Any:
+    """"""
+    return field(
+        default=kwargs.pop("default", None),
+        metadata={
+            "obj": ArgumentMetaData(
+                flags=list(flags),
+                make_flag=make_flag,
+                argument_group=group,
+                mutually_exclusive_group=mutually_exclusive_group,
+                dictionary=kwargs,
+            ),
+            "subparser": subparser,
+        },
     )
-    parameter_section = "\n".join(template.splitlines()[parameter_section_init_index:])
-    for _ in range(parameter_number - 1):
-        template += f"{parameter_section}\n"
-    for place_holder in place_holders:
-        template = template.replace(f"{{{{{place_holder}}}}}", "(.*?)")
-    template += "(.*)"
-    match = re.match(template, docstring, re.DOTALL)
-    if match:
-        matches: tuple[str, ...] = match.groups()
-        docstring_data = DocstringData(
-            matches[place_holders["description"][0]], matches[place_holders["epilog"][0]]
-        )
-        for i in range(parameter_number):
-            docstring_data.helps[
-                matches[place_holders["parameter_name"][0] + parameter_section_length * i]
-            ] = matches[
-                place_holders["parameter_description"][0] + parameter_section_length * i
-            ].strip()
-        return docstring_data
-    return None
+
+
+def get_metadata_from_field(field: Field[Any]) -> ArgumentData:
+    data: ArgumentData = ArgumentData(name=field.name, type=field.type)
+    if field.default is not field.default_factory:
+        data.default = field.default
+    if field.metadata:
+        data.parser = field.metadata.get("subparser", None)
+        metadata: ArgumentMetaData = field.metadata.get("obj", None)
+        data.flags = metadata.flags
+        data.make_flag = metadata.make_flag
+        data.argument_group = metadata.argument_group
+        data.mutually_exclusive_group = metadata.mutually_exclusive_group
+        data.kwargs = metadata.dictionary
+    return data
+
+
+def get_metadata_from_parameter(parameter: Parameter) -> ArgumentData:
+    data: ArgumentData = ArgumentData(name=parameter.name)
+    if parameter.default != parameter.empty:
+        data.default = parameter.default
+    if parameter.annotation != parameter.empty:
+        if callable(parameter.annotation):
+            data.type = parameter.annotation
+        if hasattr(parameter.annotation, "__metadata__"):
+            data.type = parameter.annotation.__origin__
+            metadata: ArgumentMetaData = parameter.annotation.__metadata__
+            if isinstance(metadata, ArgumentMetaData):
+                data.flags = metadata.flags
+                data.make_flag = metadata.make_flag
+                data.argument_group = metadata.argument_group
+                data.mutually_exclusive_group = metadata.mutually_exclusive_group
+                data.kwargs = metadata.dictionary
+    return data
+
+
+teste: tuple[str, ...] = (565,)
