94f764e92d9e64d1d94f68f8fc2e7c0ddfc3484d
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-02 00:24:12 -0300

    📄 docs: update auxliar script

--- docs/sphinx/source/convert_notebook.py
+++ docs/sphinx/source/convert_notebook.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 import os
+
+os.chdir(Path(__file__).parent)
 
 os.system("jupyter nbconvert notebooks/userguide.ipynb --to markdown --output userguide.md")
 
@@ -7,30 +10,58 @@ with open("notebooks/userguide.md", "r", encoding="utf-8") as file:
 
 text = text.replace("```python\n! python", "```\n> python")
 
+with open("temp.md", "w", encoding="utf-8") as file:
+    file.write(text)
+
 lines = text.split("\n")
 
-on_snippet = False
+on_shell_snippet = False
+on_python_snippet = False
+on_python_snippet_decorator = False
 snippet_started = False
 for i, line in enumerate(lines):
     if line.strip().startswith("Couldn't find"):
         lines[i] = "<must_remove>"
     if line.startswith("%%python"):
+        on_python_snippet = False
         lines[i] = "<must_remove>"
     if line.startswith("```bash") or line.startswith("> python"):
-        on_snippet = True
+        on_shell_snippet = True
         continue
-    if on_snippet:
-        if line.startswith("```"):
-            lines[i] = "<must_remove>"
+    if line.startswith("```python"):
+        on_python_snippet = True
+        continue
+    if on_python_snippet:
+        if line.startswith(" "):
+            lines[i] = "... " + lines[i]
             continue
-        if not line.startswith("    "):
-            if snippet_started:
-                lines[i] = "```"
-                on_snippet = False
+        if line.startswith("@"):
+            lines[i] = ">>> " + lines[i]
+            on_python_snippet_decorator = True
+            continue
+        if on_python_snippet_decorator:
+            lines[i] = "... " + lines[i]
+            on_python_snippet_decorator = False
+            continue
+        if len(line) == 0:
+            lines[i] = "... " + lines[i]
+            continue
+        if line.startswith("```"):
+            on_python_snippet = False
+            continue
+        lines[i] = ">>> " + lines[i]
+    if on_shell_snippet:
+        if line.startswith("```"):  # end of notebook snippet containing single line with cli command
+            lines[i] = "<must_remove>"  # remove this single line
+            continue
+        if not line.startswith("    "):  # end or start of output snippet
+            if snippet_started:  # end of output snippet, because it already started
+                lines[i] = "```"  # finalize output snippet
+                on_shell_snippet = False
                 snippet_started = False
                 continue
             else:
-                snippet_started = True
+                snippet_started = True  # start of output snippet
                 continue
 
 
