30294658f120c0cd1023c49dfbc53a7ee44d1a2f
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-01 21:07:43 -0300

    📄 docs: update `write_examples` script

--- docs/sphinx/source/notebooks/write_examples.py
+++ docs/sphinx/source/notebooks/write_examples.py
@@ -17,16 +17,18 @@ for i, cell in enumerate(cells):
             source[1] = f'# example{example_number if example_number >= 0 else ""}.py\n'
             filename = source[1].lstrip("# ").strip()
             with open(filename, "w", encoding="utf-8") as file:
+                file.write("import sys\nfrom pathlib import Path\npath = Path(__file__).parent\n")
+                file.write('sys.path.insert(0, str((path / "../../../../src").resolve()))')
                 file.write("".join(source[1:]))
         elif source and source[0].startswith("! python"):
             parts = source[0].split(".py")
             parts[0] = f'! python example{example_number if example_number >= 0 else ""}.py'
             source[0] = "".join(parts)
         if source:
-            cell["source"] = source
-            cells[i] = cell
+            cell["source"] = source  # update source
+            cells[i] = cell  # update cell
 
-content["cells"] = cells
+content["cells"] = cells  # update cells
 
 with open("userguide.ipynb", "w", encoding="utf-8") as file:
     json.dump(content, file, indent=4)
