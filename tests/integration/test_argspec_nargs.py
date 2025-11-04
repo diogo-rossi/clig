f4c382703da0980a75f1864b7c34d30b83f3d1fa
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-03 22:02:47 -0300

    ✅ test: add new test

--- tests/integration/test_argspec_nargs.py
+++ tests/integration/test_argspec_nargs.py
@@ -60,3 +60,83 @@ def test_nargs_questionMark_default():
     assert clig.run(bar, ["rocky", "--size", "123"]) == {"name": "rocky", "size": 123}  # optional passed
     assert clig.run(bar, ["rocky", "--size"]) == {"name": "rocky", "size": 456}  # pass no value produce const
     assert clig.run(bar, ["rocky"]) == {"name": "rocky", "size": 789}  # not passed, produce default
+
+
+def test_nargs_starMark_nonDefault():
+    """Ref: https://docs.python.org/3/library/argparse.html#nargs"""
+
+    # original behavior
+
+    parser = ArgumentParser("foo")
+    parser.add_argument(dest="name")
+    parser.add_argument(dest="size", nargs="*", type=int)
+
+    args = parser.parse_args(["rocky", "123", "456"])  # positional passed
+    assert {"name": args.name, "size": args.size} == {"name": "rocky", "size": [123, 456]}
+
+    args = parser.parse_args(["rocky"])  # not passed, produce empty list
+    assert {"name": args.name, "size": args.size} == {"name": "rocky", "size": []}
+
+    # test of lib
+
+    def foo(name: str, size: Arg[list[int], data(nargs="*")]):
+        return locals()
+
+    assert clig.run(foo, ["rocky", "123", "456"]) == {"name": "rocky", "size": [123, 456]}  # passed
+    assert clig.run(foo, ["rocky"]) == {"name": "rocky", "size": []}  # not passed, produce empty list
+
+    # same but with type = tuple
+
+    def bar(name: str, size: Arg[tuple[int], data(nargs="*")]):
+        return locals()
+
+    assert clig.run(bar, ["rocky", "123", "456"]) == {"name": "rocky", "size": (123, 456)}  # passed
+    assert clig.run(bar, ["rocky"]) == {"name": "rocky", "size": ()}  # not passed, produce empty list
+
+
+def test_nargs_starMark_default():
+    """Ref: https://docs.python.org/3/library/argparse.html#nargs"""
+
+    # original behavior
+    # note that, in this case, to pass a normal nargs="*" produces list, but the default is integer
+
+    parser = ArgumentParser("foo")
+    parser.add_argument(dest="name")
+    parser.add_argument(dest="size", nargs="*", default=789, type=int)
+
+    args = parser.parse_args(["rocky", "123", "456"])  # positional passed
+    assert {"name": args.name, "size": args.size} == {"name": "rocky", "size": [123, 456]}
+
+    args = parser.parse_args(["rocky"])  # not passed, produce empty list
+    assert {"name": args.name, "size": args.size} == {"name": "rocky", "size": 789}
+
+    # test of lib
+    # here is the same: a normal nargs="*" produces list, but the default is integer
+
+    def foo(name: str, size: Arg[int, data(nargs="*")] = 789):
+        return locals()
+
+    assert clig.run(foo, ["rocky", "--size", "123", "456"]) == {"name": "rocky", "size": [123, 456]}  # passed
+    assert clig.run(foo, ["rocky"]) == {"name": "rocky", "size": 789}  # not passed, produce empty list
+
+    # same but with type = tuple
+    # in this case, the default is intentionally passed as tuple
+
+    def bar(name: str, size: Arg[tuple[int], data(nargs="*")] = (789,)):
+        return locals()
+
+    assert clig.run(bar, ["rocky", "--size", "123", "456"]) == {"name": "rocky", "size": (123, 456)}  # passed
+    assert clig.run(bar, ["rocky"]) == {"name": "rocky", "size": (789,)}  # not passed, produce empty list
+
+    # the same above is achieved with default=789 in data() function
+
+    def baz(name: str, size: Arg[int, data(nargs="*", default=789)]):
+        return locals()
+
+    def ham(name: str, size: Arg[tuple[int], data(nargs="*", default=(789,))]):
+        return locals()
+
+    assert clig.run(baz, ["rocky", "--size", "123", "456"]) == {"name": "rocky", "size": [123, 456]}  # passed
+    assert clig.run(baz, ["rocky"]) == {"name": "rocky", "size": 789}  # not passed, produce empty list
+    assert clig.run(ham, ["rocky", "--size", "123", "456"]) == {"name": "rocky", "size": (123, 456)}  # passed
+    assert clig.run(ham, ["rocky"]) == {"name": "rocky", "size": (789,)}  # not passed, produce empty list
