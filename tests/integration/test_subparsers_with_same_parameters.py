31cf1393a42ec155632f7c07abc29c0aed04b79e
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-05 00:01:45 -0300

    ✅ test: add original behavior

--- tests/integration/test_subparsers_with_same_parameters.py
+++ tests/integration/test_subparsers_with_same_parameters.py
@@ -6,9 +6,30 @@ sys.path.insert(0, str((path).resolve()))
 sys.path.insert(0, str((path / "../../src").resolve()))
 
 from clig import Command, Arg, data
+from argparse import ArgumentParser, Namespace
 
 
 def test_subparsers_with_same_parameters_all_kw():
+
+    # original lib has an issue
+
+    parser = ArgumentParser()
+    parser.add_argument("--foo")
+    subs = parser.add_subparsers()
+    subparser = subs.add_parser("sub")
+    subparser.add_argument("--foo")
+    subsubs = subparser.add_subparsers()
+    subsubparser = subsubs.add_parser("subsub")
+    subsubparser.add_argument("--foo")
+
+    assert parser.parse_args(["--foo", "yoco"]) == Namespace(foo="yoco")
+    assert parser.parse_args(["--foo", "yoco", "sub", "--foo", "rocky"]) == Namespace(foo="rocky")
+    assert parser.parse_args(
+        ["--foo", "yoco", "sub", "--foo", "rocky", "subsub", "--foo", "sand"]
+    ) == Namespace(foo="sand")
+
+    # issue solved
+
     def maincmd(foo: str | None = None):
         assert foo == "yoco"
 
