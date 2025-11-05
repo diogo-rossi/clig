fb81e2666e4812c041838417ddd5d2d9ae2dca33
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-05 13:26:12 -0300

    ✅ test: update tests

--- tests/integration/test_intg_list_tuple_union.py
+++ tests/integration/test_intg_list_tuple_union.py
@@ -13,7 +13,7 @@ sys.path.insert(0, str((path / "../../src").resolve()))
 # %%          Resources
 ##############################################################################################################
 import pytest
-from typing import Protocol
+from typing import Protocol, Sequence
 
 
 class OutErr(Protocol):
@@ -25,35 +25,22 @@ class CapSys(Protocol):
     def readouterr(self) -> OutErr: ...
 
 
+from clig import Command, Arg, data
+
 ##############################################################################################################
-# %%          Testing
+# %%          LISTS
 ##############################################################################################################
 
 
-from clig import Command, Arg, data, run
-
-
-def test_list_withDefault_turnsIntoNargsStar():
-    def main(names: Arg[list[str], data(default="rocky")] = []):
+def test_list_noDefaultOnFunction_turnsIntoPositionalNargsPlus(capsys: CapSys):
+    def main(names: list[str]):
         return locals()
 
-    assert Command(main).run(["--names", "tony", "neo"]) == {"names": ["tony", "neo"]}
-    assert Command(main).run([]) == {"names": "rocky"}
+    cmd = Command(main)
 
-
-def test_tuple_withDefault_turnsIntoNargsStar():
-    def main(names: Arg[tuple[str, ...], data(default=("rocky",))] = ()):
-        return locals()
-
-    assert Command(main).run(["--names", "tony", "neo"]) == {"names": ("tony", "neo")}
-    assert Command(main).run([]) == {"names": ("rocky",)}
-
-
-def test_list_defaultOnData_noDefaultOnFunction_turnsIntoNargsPlus(capsys: CapSys):
-    def main(names: Arg[list[str], data(default="rocky")]):
-        return locals()
-
-    assert Command(main).run(["tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.run(["tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.arguments[0].nargs == "+"
+    assert cmd.arguments[0].default == None  # standard default in argparse
 
     with pytest.raises(SystemExit) as e:
         Command(main).run([])
@@ -63,32 +50,221 @@ def test_list_defaultOnData_noDefaultOnFunction_turnsIntoNargsPlus(capsys: CapSy
     assert "the following arguments are required: names" in output
 
 
-def test_tuple_defaultOnData_noDefaultOnFunction_turnsIntoNargsN(capsys: CapSys):
-    def main(names: Arg[tuple[str, str], data(default="rocky")]):
+def test_list_defaultOnData_noDefaultOnFunction_turnsIntoPositionalNargsPlus(capsys: CapSys):
+    def main(names: Arg[list[str], data(default="rocky")]):
         return locals()
 
-    assert Command(main).run(["tony", "neo"]) == {"names": ("tony", "neo")}
+    cmd = Command(main)
+
+    assert cmd.run(["tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.arguments[0].nargs == "+"
+    assert cmd.arguments[0].default == "rocky"
 
     with pytest.raises(SystemExit) as e:
         Command(main).run([])
 
     assert e.value.code == 2  # argparse exits with code 2 for argument errors
     output = capsys.readouterr().err
+    assert "the following arguments are required: names" in output
 
-    assert "the following arguments are required: name" in output
 
-
-def test_union_tuple_none_withDefaultNone():
-    def foo(ages: tuple[int, int] | None = None):
+def test_list_withDefault_turnsIntoFlaggedNargsStar():
+    def main(names: list[str] = []):
         return locals()
 
-    assert Command(foo).run(["--ages", "36", "64"]) == {"ages": (36, 64)}
-    assert Command(foo).run([]) == {"ages": None}
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.run([]) == {"names": []}
+    assert cmd.arguments[0].nargs == "*"
 
 
-def test_union_list_none_withDefaultNone():
+def test_list_defaultOnData_withDefault_turnsIntoFlaggedNargsStar():
+    def main(names: Arg[list[str], data(default="rocky")] = []):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.run([]) == {"names": "rocky"}
+    assert cmd.arguments[0].nargs == "*"
+
+
+def test_unionListNone_withDefaultNone_turnsIntoFlaggedNargsStar():
     def foo(ages: list[int] | None = None):
         return locals()
 
-    assert Command(foo).run(["--ages", "36", "64", "42"]) == {"ages": [36, 64, 42]}
-    assert Command(foo).run([]) == {"ages": None}
+    cmd = Command(foo)
+
+    assert cmd.run(["--ages", "36", "64", "42"]) == {"ages": [36, 64, 42]}
+    assert cmd.run([]) == {"ages": None}
+    assert cmd.arguments[0].nargs == "*"
+
+
+##############################################################################################################
+# %%          TUPLES
+##############################################################################################################
+
+
+def test_tuple_noDefaultOnFunction_turnsIntoPositionalNargsN(capsys: CapSys):
+    def main(names: tuple[str, str]):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["tony", "neo"]) == {"names": ("tony", "neo")}
+    assert cmd.arguments[0].nargs == 2
+    assert cmd.arguments[0].default == None  # standard default in argparse
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run([])
+
+    assert e.value.code == 2  # argparse exits with code 2 for argument errors
+    output = capsys.readouterr().err
+
+    assert "the following arguments are required: name" in output
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(["tony", "neo", "jean"])
+
+    assert e.value.code == 2  # argparse exits with code 2 for argument errors
+    output = capsys.readouterr().err
+
+    assert "unrecognized arguments: jean" in output
+
+
+def test_tuple_defaultOnData_noDefaultOnFunction_turnsIntoPositionalNargsN(capsys: CapSys):
+    def main(names: Arg[tuple[str, str], data(default="rocky")]):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["tony", "neo"]) == {"names": ("tony", "neo")}
+    assert cmd.arguments[0].nargs == 2
+    assert cmd.arguments[0].default == "rocky"  # give default in data (does not make difference here)
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run([])
+
+    assert e.value.code == 2  # argparse exits with code 2 for argument errors
+    output = capsys.readouterr().err
+
+    assert "the following arguments are required: name" in output
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(["tony", "neo", "jean"])
+
+    assert e.value.code == 2  # argparse exits with code 2 for argument errors
+    output = capsys.readouterr().err
+
+    assert "unrecognized arguments: jean" in output
+
+
+def test_tuple_withDefault_turnsIntoFlaggedNargsN():
+    def main(names: tuple[str, str] = ("tommy", "caco")):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ("tony", "neo")}
+    assert cmd.run([]) == {"names": ("tommy", "caco")}
+    assert cmd.arguments[0].nargs == 2
+
+
+def test_tuple_withDefault_withDefaultOnData_turnsIntoFlaggedNargsN(capsys: CapSys):
+    def main(names: Arg[tuple[str, str], data(default="rocky")] = ("tommy", "caco")):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ("tony", "neo")}
+    assert cmd.run([]) == {"names": "rocky"}
+    assert cmd.arguments[0].nargs == 2
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(["--names", "tony", "neo", "jean"])
+
+    assert e.value.code == 2  # argparse exits with code 2 for argument errors
+    output = capsys.readouterr().err
+
+    assert "unrecognized arguments: jean" in output
+
+
+def test_tupleEllipisis_withDefault_turnsIntoFlaggedNargsStar():
+    def main(names: Arg[tuple[str, ...], data(default="rocky")] = ()):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ("tony", "neo")}
+    assert cmd.run(["--names", "tony", "neo", "jean"]) == {"names": ("tony", "neo", "jean")}
+    assert cmd.run([]) == {"names": "rocky"}
+    assert cmd.arguments[0].nargs == "*"
+
+
+def test_unionTupleNone_withDefaultNone_turnsIntoFlaggedNargsN():
+    def foo(ages: tuple[int, int] | None = None):
+        return locals()
+
+    cmd = Command(foo)
+
+    assert cmd.run(["--ages", "36", "64"]) == {"ages": (36, 64)}
+    assert cmd.run([]) == {"ages": None}
+    assert cmd.arguments[0].nargs == 2
+
+
+##############################################################################################################
+# %%          SEQUENCES
+##############################################################################################################
+
+
+def test_Sequence_withDefault_turnsIntoFlaggedNargsStar():
+    def main(names: Sequence[str] = []):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.run([]) == {"names": []}
+    assert cmd.arguments[0].nargs == "*"
+
+
+def test_Sequence_withDefaultAndDefaultOnData_turnsIntoFlaggedNargsStar():
+    def main(names: Arg[Sequence[str], data(default="rocky")] = []):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["--names", "tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.run([]) == {"names": "rocky"}
+    assert cmd.arguments[0].nargs == "*"
+
+
+def test_unionSequenceNone_withDefaultNone_turnsIntoFlaggedNargsStar():
+    def foo(ages: Sequence[int] | None = None):
+        return locals()
+
+    cmd = Command(foo)
+
+    assert cmd.run(["--ages", "36", "64", "42"]) == {"ages": [36, 64, 42]}
+    assert cmd.run([]) == {"ages": None}
+    assert cmd.arguments[0].nargs == "*"
+
+
+def test_Sequence_defaultOnData_noDefaultOnFunction_turnsIntoPositionalNargsPlus(capsys: CapSys):
+    def main(names: Arg[Sequence[str], data(default="rocky")]):
+        return locals()
+
+    cmd = Command(main)
+
+    assert cmd.run(["tony", "neo"]) == {"names": ["tony", "neo"]}
+    assert cmd.run(["tony", "neo", "jean"]) == {"names": ["tony", "neo", "jean"]}
+    assert cmd.arguments[0].nargs == "+"
+    assert cmd.arguments[0].default == "rocky"
+
+    with pytest.raises(SystemExit) as e:
+        Command(main).run([])
+
+    assert e.value.code == 2  # argparse exits with code 2 for argument errors
+    output = capsys.readouterr().err
+    assert "the following arguments are required: names" in output
