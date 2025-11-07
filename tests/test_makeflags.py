3b878aeeb73fabe3841c8abf9e7dbb7e1d0a48ae
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-07 10:03:03 -0300

    ✅ test: add test to new feature

--- tests/test_makeflags.py
+++ tests/test_makeflags.py
@@ -1,3 +1,7 @@
+##############################################################################################################
+# %%          Add `<root>/src` to sys.path
+##############################################################################################################
+
 import sys
 from pathlib import Path
 
@@ -5,6 +9,27 @@ this_dir = Path(__file__).parent
 
 sys.path.insert(0, str((this_dir).resolve()))
 sys.path.insert(0, str((this_dir / "../src").resolve()))
+
+##############################################################################################################
+# %%          Resources
+##############################################################################################################
+import pytest
+from typing import Protocol
+import argparse
+
+
+class OutErr(Protocol):
+    out: str
+    err: str
+
+
+class CapSys(Protocol):
+    def readouterr(self) -> OutErr: ...
+
+
+##############################################################################################################
+# %%          Tests
+##############################################################################################################
 
 from clig.clig import Arg, data, Command
 
@@ -81,3 +106,50 @@ def test_conflict_force_make_flags_true_on_command():
     cmd._add_parsers()
     assert cmd.arguments[0].option_strings == ["-f", "--foo"]
     assert cmd.arguments[1].option_strings == ["-b", "--baz", "--bazham"]
+
+
+def test_force_make_flags_on_command_argument_not_annotated(capsys: CapSys):
+    def main(foobar: str, bazham: int = 42):
+        return locals()
+
+    cmd = Command(main, make_flags=False)
+    cmd._add_parsers()
+    assert cmd.arguments[0].option_strings == []
+    assert cmd.arguments[1].option_strings == []
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(["dio"])
+
+    assert e.value.code == 2
+    assert "the following arguments are required: bazham" in capsys.readouterr().err
+
+
+def test_force_make_shorts_on_command():
+    def main(foobar: str = "dio", bazham: int = 42):
+        return locals()
+
+    cmd = Command(main)
+    cmd._add_parsers()
+    assert cmd.arguments[0].option_strings == ["--foobar"]
+    assert cmd.arguments[1].option_strings == ["--bazham"]
+
+    cmd = Command(main, make_shorts=True)
+    cmd._add_parsers()
+    assert cmd.arguments[0].option_strings == ["-f", "--foobar"]
+    assert cmd.arguments[1].option_strings == ["-b", "--bazham"]
+
+
+def test_force_make_shorts_conflict(capsys: CapSys):
+    def main(foo: str = "dio", foobar: int = 42):
+        return locals()
+
+    cmd = Command(main)
+    cmd._add_parsers()
+    assert cmd.arguments[0].option_strings == ["--foo"]
+    assert cmd.arguments[1].option_strings == ["--foobar"]
+
+    with pytest.raises(argparse.ArgumentError) as e:
+        cmd = Command(main, make_shorts=True)
+        cmd._add_parsers()
+
+    assert "conflicting option string: -f" in e.value.message
