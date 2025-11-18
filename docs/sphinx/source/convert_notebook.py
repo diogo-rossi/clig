185989d2bbccc93fe7c76237f62a3cb9f7c2486d
Diogo Rossi <rossi.diogo@gmail.com>
2025-11-17 21:06:54 -0300

    📄 docs: wip

--- docs/sphinx/source/convert_notebook.py
+++ docs/sphinx/source/convert_notebook.py
@@ -1,105 +1,134 @@
 from pathlib import Path
 import os
 
-os.chdir(Path(__file__).parent)
+THISDIR: Path = Path(__file__).parent
 
-os.system("jupyter nbconvert notebooks/userguide.ipynb --to markdown --output userguide.md")
-
-with open("notebooks/userguide.md", "r", encoding="utf-8") as file:
-    text = file.read()
-
-text = text.replace("```python\n! python", "```\n> python")
+os.chdir(THISDIR)
 
 
-lines = text.split("\n")
+def convert_jupyter_notebook_to_markdown(notebook_name: str):
+    os.system(f"jupyter nbconvert notebooks/{notebook_name}.ipynb --to markdown --output {notebook_name}.md")
 
-on_shell_snippet = False
-on_python_snippet = False
-on_python_snippet_output = False
-end_of_python_snippet_output = False
-on_python_snippet_decorator = False
-snippet_started = False
-in_error = False
-for i, line in enumerate(lines):
-    if "Traceback (most recent call last):" in line:
-        in_error = True
-        lines[i] = "<must_remove>"
-        continue
-    if line.strip().startswith("ValueError:") or line.strip().startswith("TypeError:"):
-        in_error = False
-    if in_error:
-        lines[i] = "<must_remove>"
-        continue
-    if line.strip().startswith("Couldn't find"):
-        lines[i] = "<must_remove>"
-    if line.startswith("%%python"):
-        on_python_snippet = False
-        lines[i] = "<must_remove>"
-    if line.startswith("```bash") or line.startswith("> python"):
-        on_shell_snippet = True
-        continue
-    if line.startswith("```python"):
-        on_python_snippet = True
-        continue
-    if on_python_snippet:
-        if line.startswith("```"):  # end of snippet containing python code. Need to continue for output
-            lines[i] = "<must_remove>"  # remove this single line
-            on_python_snippet = False  # end of snippet containing python code
-            on_python_snippet_output = True  # the next is the output
+
+def line_with_raised_error(line: str):
+    return any([line.strip().startswith(error) for error in ["ValueError:", "TypeError:"]])
+
+
+def format_exported_notebook_file(input_file_name: str, output_file_name: str):
+
+    with open(f"notebooks/{input_file_name}.md", "r", encoding="utf-8") as file:
+        text = file.read()
+
+    # format shell cells
+    text = text.replace("```python\n! python", "```\n> python")
+
+    lines = text.split("\n")
+
+    on_shell_snippet = False
+    on_python_snippet = False
+    on_python_snippet_output = False
+    end_of_python_snippet_output = False
+    on_python_snippet_decorator = False
+    snippet_started = False
+    in_error = False
+    for i, line in enumerate(lines):
+
+        if "Traceback (most recent call last):" in line:  # check if entered in error cells
+            in_error = True
+            lines[i] = "<must_remove>"
             continue
-        if line.startswith(" "):
-            lines[i] = "... " + lines[i]
+
+        if line_with_raised_error(line):  # check if exited error cells
+            in_error = False
+
+        if line.startswith("```python"):  # check if entered python snippet cell
+            on_python_snippet = True
             continue
-        if line.startswith("@"):
-            lines[i] = ">>> " + lines[i]
-            on_python_snippet_decorator = True
+
+        if line.startswith("> python"):  # check if entered shell snippet cell
+            on_shell_snippet = True
             continue
-        if on_python_snippet_decorator:
-            lines[i] = "... " + lines[i]
-            on_python_snippet_decorator = False
-            continue
-        if len(line) == 0:
-            lines[i] = "... " + lines[i]
-            continue
-        if line.startswith("```"):
+
+        if line.startswith("%%python"):  # The python snippet is a file snippet, not python snippet
             on_python_snippet = False
+            lines[i] = "<must_remove>"
+
+        if in_error:  # remove error cell
+            lines[i] = "<must_remove>"
             continue
-        lines[i] = ">>> " + lines[i]
-    if on_python_snippet_output:
-        if not line.startswith("    "):  # end or start of output snippet
-            if end_of_python_snippet_output:  # end of output snippet
-                lines[i] = "```"  # finalize output snippet
-                on_python_snippet_output = False
-                end_of_python_snippet_output = False
-                continue
-            else:
+
+        if on_python_snippet:
+            if line.startswith("```"):  # end of snippet containing python code. Need to continue for output
                 lines[i] = "<must_remove>"  # remove this single line
-                end_of_python_snippet_output = True  # the next will be the end of output snippet
+                on_python_snippet = False  # end of snippet containing python code
+                on_python_snippet_output = True  # the next is the output
                 continue
-        lines[i] = lines[i].strip()
-        # else:
-        #     lines[i] = lines[i].strip()
-        #     if len(lines[i]) == 0:
-        #         lines[i] = "<must_remove>"
-    if on_shell_snippet:
-        if line.startswith("```"):  # end of notebook snippet containing single line with cli command
-            lines[i] = "<must_remove>"  # remove this single line
-            continue
-        if not line.startswith("    "):  # end or start of output snippet
-            if snippet_started:  # end of output snippet, because it already started
-                lines[i] = "```"  # finalize output snippet
-                on_shell_snippet = False
-                snippet_started = False
+            if line.startswith(" "):  # the line is indented, put '...'
+                lines[i] = "... " + lines[i]
                 continue
-            else:
-                snippet_started = True  # start of output snippet
+            if line.startswith("@"):  # the line contains a decorator, put '>>>' and start decorator block
+                lines[i] = ">>> " + lines[i]
+                on_python_snippet_decorator = True
                 continue
+            if on_python_snippet_decorator:  # the line is not indented but is in a decorator block, put '...'
+                lines[i] = "... " + lines[i]
+                on_python_snippet_decorator = False
+                continue
+            if len(line) == 0:  # the line is empty, put '...'
+                lines[i] = "... " + lines[i]
+                continue
+            lines[i] = ">>> " + lines[i]  # the line is a common python command
+
+        if on_python_snippet_output:
+            if not line.startswith("    "):  # end or start of output snippet
+                if end_of_python_snippet_output:  # end of output snippet
+                    lines[i] = "```"  # finalize output snippet
+                    on_python_snippet_output = False
+                    end_of_python_snippet_output = False
+                    continue
+                else:
+                    lines[i] = "<must_remove>"  # remove this single line
+                    end_of_python_snippet_output = True  # the next indented will be the end of output snippet
+                    continue
+            lines[i] = lines[i].strip()
+
+        if on_shell_snippet:
+            if line.startswith("```"):  # end of notebook snippet containing single line with cli command
+                lines[i] = "<must_remove>"  # remove this single line
+                continue
+            if not line.startswith("    "):  # end or start of output snippet
+                if snippet_started:  # end of output snippet, because it already started
+                    lines[i] = "```"  # finalize output snippet
+                    on_shell_snippet = False
+                    snippet_started = False
+                    continue
+                else:
+                    snippet_started = True  # start of output snippet
+                    continue
+    # remove lines
+    text = "\n".join([line for line in lines if line != "<must_remove>"]).replace("... \n...", "...")
+
+    # merge snippets
+    text = text.replace("\n```\n\n```python\n>>>", ">>>")
+
+    with open(f"notebooks/{output_file_name}.md", "w", encoding="utf-8") as file:
+        file.write(text)
 
 
-text = "\n".join([line for line in lines if line != "<must_remove>"]).replace("... \n...", "...")
-text = text.replace("\n```\n\n```python\n>>>", ">>>")
-with open("notebooks/userguide.md", "w", encoding="utf-8") as file:
-    file.write(text)
+convert_jupyter_notebook_to_markdown("userguide")
+
+format_exported_notebook_file("userguide", "temp_userguide")
+
+# if line.startswith("```"):  # end of snippet containing python code
+#     on_python_snippet = False
+#     continue
+# else:
+#     lines[i] = lines[i].strip()
+#     if len(lines[i]) == 0:
+#         lines[i] = "<must_remove>"
+# if line.strip().startswith("Couldn't find"):
+#     lines[i] = "<must_remove>"
+
 
 # with open("../../../src/clig/__init__.pyi", "w", encoding="utf-8") as file:
 #     file.write(
