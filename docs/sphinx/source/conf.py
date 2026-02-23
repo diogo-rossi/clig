36551901638a73524315430885732ecfaced405a
Diogo Rossi <rossi.diogo@gmail.com>
2026-02-22 22:52:28 -0300

    📄 docs: update conf.py

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -1,28 +1,18 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# %%          IMPORTS AND SETTINGS
+############# IMPORTS AND SETTINGS ###########################################################################
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-from datetime import datetime
-
-project = "clig"
-copyright = f"{datetime.now().year}, Diogo Rossi"
-author = "Diogo Rossi"
-release = "0.6.0"
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-import os
-import sys
 import importlib
 import inspect
+import os
+from datetime import datetime
 from pathlib import Path
-from typing import TypedDict, Literal
+from typing import Literal, TypedDict
+
 import git
+from pyprj import nbmd
+from pyprj.markdown_utils import get_markdown_sections
+from pyprj.pyproject import author_name, pkg_name, pkg_version, pyproject
+
 import clig
 
 
@@ -31,14 +21,54 @@ class PyDomainInfo(TypedDict):
     fullname: str
 
 
-path = Path(__file__).parent
-sys.path.insert(0, str((path).resolve()))
-os.chdir(path)
+THIS_DIR: Path = Path(__file__).parent.resolve()
+os.chdir(THIS_DIR)
+
+notebooks_dirpath: Path = Path("./notebooks")
+
+nbmd.nbmd(notebooks_dirpath, dont_run_notebooks_before=False)
+
+INITIAL_README: str = """# `clig` - CLI Generator
+
+A single module, pure python, **Command Line Interface Generator**.
+
+OBS: currently under development.
+
+"""
+
+
+index_sections: list[str] = get_markdown_sections(Path("index.md"))
+userguide_sections: list[str] = get_markdown_sections(notebooks_dirpath / "userguide.md")
+
+
+readme: str = (
+    (INITIAL_README + index_sections[2] + "".join(userguide_sections))
+    .replace(
+        "./advancedfeatures.md",
+        "https://github.com/diogo-rossi/clig/blob/main/docs/sphinx/source/notebooks/advancedfeatures.md",
+    )
+    .replace("", "")
+)
+
+
+with open(pyproject.dirpath / "README.md", "w", encoding="utf-8") as file:
+    file.write(readme)
+
 
 git_repo = git.Repo(".", search_parent_directories=True)
 code_url = f"https://github.com/diogo-rossi/clig/blob/main/"
 
-import convert_notebook
+
+# %%          SPHINX DATA
+############# SPHINX DATA ####################################################################################
+
+project = pkg_name
+copyright = f"{datetime.now().year}, {author_name}"
+author = author_name
+release = pkg_version
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -52,27 +82,23 @@ extensions = [
     "sphinx.ext.linkcode",
 ]
 
-maximum_signature_line_length = 70
+templates_path = ["_templates"]
+exclude_patterns = []
 
-# Napoleon settings
+maximum_signature_line_length = 70
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 default_role = "code"
 
-templates_path = ["_templates"]
-exclude_patterns = []
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
-html_title = '<p style="text-align: center"><b>clig</b></p>'
 html_static_path = ["_static"]
-# conf.py
-html_css_files = ["css/custom.css"]
-html_logo = "logo.png"
 
+html_theme = "furo"
+html_title = f'<p style="text-align: center"><b>{pkg_name}</b></p>'
+html_css_files = ["css/custom.css"]
+html_logo = "../../logo.png"
 myst_heading_anchors = 4
 
 
