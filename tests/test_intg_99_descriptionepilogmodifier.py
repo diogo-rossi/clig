dd43d355597ad5ee93e6b01c226cba202f891abd
Diogo Rossi <rossi.diogo@gmail.com>
2026-06-20 11:51:18 -0300

    ✅ test: test fo epilog and description modifiers

---
+++ tests/test_intg_99_descriptionepilogmodifier.py
@@ -0,0 +1,76 @@
+from enum import Enum
+from resources import CapSys
+import clig
+import pytest
+
+helpmsg_append_description = """
+usage: main [-h] a b c
+
+Description of the function. Appended TEXT.
+
+positional arguments:
+  a           Description of a.
+  b           Description of b.
+  c           Description of c.
+
+options:
+  -h, --help  show this help message and exit
+
+Epilog of the function.
+""".strip()
+
+helpmsg_append_epilog = """
+usage: main [-h] a b c
+
+Description of the function.
+
+positional arguments:
+  a           Description of a.
+  b           Description of b.
+  c           Description of c.
+
+options:
+  -h, --help  show this help message and exit
+
+Epilog of the function. Appended TEXT.
+""".strip()
+
+
+def main(a, b, c) -> dict:
+    """Description of the function.
+
+    Epilog of the function.
+
+    Parameters
+    ----------
+    - `a` (`int`):
+        Description of a.
+
+    - `b` (`str`):
+        Description of b.
+
+    - `c` (`float`):
+        Description of c.
+
+    Returns
+    -------
+    `dict`:
+        The `locals()` dict.
+    """
+    return locals()
+
+
+def test_intg_99_descriptionmodifier(capsys: CapSys):
+    cmd = clig.Command(main, descriptionmodifier=lambda s: f"{s} Appended TEXT.")
+    with pytest.raises(SystemExit) as e:
+        cmd.run(["--help"])
+    assert e.value.code == 0
+    assert helpmsg_append_description.strip() in capsys.readouterr().out
+
+
+def test_intg_99_epilogmodifier(capsys: CapSys):
+    cmd = clig.Command(main, epilogmodifier=lambda s: f"{s} Appended TEXT.")
+    with pytest.raises(SystemExit) as e:
+        cmd.run(["--help"])
+    assert e.value.code == 0
+    assert helpmsg_append_epilog.strip() in capsys.readouterr().out
