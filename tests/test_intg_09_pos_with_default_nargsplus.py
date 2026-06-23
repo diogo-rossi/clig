03e0b5914128b272cda7dc29694a5c9b8564a1d5
Diogo Rossi <rossi.diogo@gmail.com>
2026-06-22 22:56:49 -0300

    ✅ test: add argparse to test and rename

--- tests/test_intg_09_pos_with_default_nargsplus.py
+++ tests/test_intg_09_pos_with_default_nargsplus.py
@@ -1,4 +1,5 @@
 import pytest
+from argparse import ArgumentParser
 from resources import CapSys
 from clig import Arg, data, run
 
@@ -12,6 +13,10 @@ def main(name: Arg[list[str], data(default="diogo")]):
     return locals()
 
 
+parser = ArgumentParser()
+parser.add_argument("name", default="diogo", nargs="+")
+
+
 def test_intg_09_pos_with_default(capsys: CapSys):
     with pytest.raises(SystemExit) as e:
         run(main, [])
@@ -19,3 +24,4 @@ def test_intg_09_pos_with_default(capsys: CapSys):
     output = capsys.readouterr().err
     assert error_msg in output
     assert run(main, ["rocky", "sandy"]) == {"name": ["rocky", "sandy"]}
+    assert run(main, ["rocky", "sandy"]) == vars(parser.parse_args(["rocky", "sandy"]))
