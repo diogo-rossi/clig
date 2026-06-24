6399aebc905d83f4e5777387eb0de4c117ac2198
Diogo Rossi <rossi.diogo@gmail.com>
2026-06-24 18:13:57 -0300

    ✅ test: new tests

--- tests/test_examples_clig_subcommands.py
+++ tests/test_examples_clig_subcommands.py
@@ -1,10 +1,11 @@
 from resources import CapSys
 import clig
-from clig import Command
+from clig import Command, command, subcommand, run
 import pytest
 
 
 def test_prog01():
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#subcommands"""
 
     def main(name: str, age: int, height: float):
         return locals()
@@ -39,6 +40,7 @@ options:
 
 
 def test_prog02(capsys: CapSys):
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#subcommands-using-methods"""
     from clig import Command
 
     def prog(name: str, age: int):
@@ -75,8 +77,248 @@ def test_prog02(capsys: CapSys):
     assert helpmsg_prog02_sub in output
 
     assert cmd.run("jack 23".split()) == {"name": "jack", "age": 23}
+    assert cmd.run("jane 46 subfunction1 1.50".split()) == {"height": 1.50}
     assert cmd.run("jack 23 subfunction2 michael suzan subsubfunction santos SP".split()) == {
         "city": "santos",
+        "state": "SP",
+    }
+
+
+helpmsg_prog03 = """
+usage: main [-h] [--verbose] {foo,bar} ...
+
+Description for the main command
+
+positional arguments:
+  {foo,bar}
+    foo       Help for foo sub command
+    bar       Help for bar sub command
+
+options:
+  -h, --help  show this help message and exit
+  --verbose
+""".strip()
+
+
+def test_prog03(capsys: CapSys):
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#subcommands-using-method-decorators"""
+
+    def main(verbose: bool = False):
+        """Description for the main command"""
+        return locals()
+
+    # create the command object
+    cmd = Command(main)
+
+    @cmd.subcommand
+    def foo(a, b):
+        """Help for foo sub command"""
+        return locals()
+
+    @cmd.subcommand
+    def bar(c, d):
+        """Help for bar sub command"""
+        return locals()
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog03 in output
+    assert cmd.run(args=["--verbose"]) == {"verbose": True}
+    assert cmd.run(args=["foo", "baz", "ham"]) == {"a": "baz", "b": "ham"}
+    assert cmd.run(args=["bar", "jack", "chan"]) == {"c": "jack", "d": "chan"}
+
+
+helpmsg_prog04_main = """
+usage: prog [-h] [--name NAME] [--age AGE] {subfunction1,subfunction2} ...
+
+positional arguments:
+  {subfunction1,subfunction2}
+
+options:
+  -h, --help            show this help message and exit
+  --name NAME
+  --age AGE
+""".strip()
+
+helpmsg_prog04_subfunction2 = """
+usage: prog subfunction2 [-h] father mother {subsubfunction} ...
+
+positional arguments:
+  father
+  mother
+  {subsubfunction}
+
+options:
+  -h, --help        show this help message and exit
+""".strip()
+
+
+def test_prog_04(capsys: CapSys):
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#adding-internal-level-of-subcommads-with-decorators"""
+
+    def prog(name: str = "mario", age: int = 40):
+        return locals()
+
+    cmd = Command(prog)
+
+    @cmd.subcommand
+    def subfunction1(height: float):
+        return locals()
+
+    @cmd.subcommand
+    def subfunction2(father: str, mother: str):
+        return locals()
+
+    @cmd.subcommands["subfunction2"].subcommand
+    def subsubfunction(city: str, state: str):
+        return locals()
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog04_main in output
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["subfunction2", "-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog04_subfunction2 in output
+
+    assert cmd.run([]) == {"name": "mario", "age": 40}
+    assert cmd.run(["subfunction1", "1.70"]) == {"height": 1.70}
+    assert cmd.run(["subfunction2", "jose", "maria"]) == {"father": "jose", "mother": "maria"}
+    assert cmd.run(["subfunction2", "jose", "maria", "subsubfunction", "limeira", "SP"]) == {
+        "city": "limeira",
+        "state": "SP",
+    }
+
+
+helpmsg_prog05_main = """
+usage: prog [-h] [--name NAME] [--age AGE] {subfunction} ...
+
+positional arguments:
+  {subfunction}
+
+options:
+  -h, --help     show this help message and exit
+  --name NAME
+  --age AGE
+""".strip()
+
+helpmsg_prog05_subfunction = """
+usage: prog subfunction [-h] height {internalsubfunction} ...
+
+positional arguments:
+  height
+  {internalsubfunction}
+
+options:
+  -h, --help            show this help message and exit
+""".strip()
+
+
+def test_prog05(capsys: CapSys):
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#method-decorator-with-arguments"""
+
+    def prog(name: str = "mario", age: int = 40):
+        return locals()
+
+    cmd = Command(prog)
+
+    @cmd.subcommand
+    def subfunction(height: float):
+        return locals()
+
+    @cmd.subcommand(parent=subfunction)
+    def internalsubfunction(city: str, state: str):
+        return locals()
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog05_main in output
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["subfunction", "-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog05_subfunction in output
+
+    assert cmd.run([]) == {"name": "mario", "age": 40}
+    assert cmd.run(["subfunction", "1.70"]) == {"height": 1.70}
+    assert cmd.run(["subfunction", "1.70", "internalsubfunction", "limeira", "SP"]) == {
+        "city": "limeira",
+        "state": "SP",
+    }
+
+
+helpmsg_prog05_main_with_name_as_parent = """
+usage: prog [-h] [--name NAME] [--age AGE] {subprog} ...
+
+positional arguments:
+  {subprog}
+
+options:
+  -h, --help   show this help message and exit
+  --name NAME
+  --age AGE
+""".strip()
+
+helpmsg_prog05_subfunction_with_name_as_parent = """
+usage: prog subprog [-h] height {internalsubfunction} ...
+
+positional arguments:
+  height
+  {internalsubfunction}
+
+options:
+  -h, --help            show this help message and exit
+""".strip()
+
+
+def test_prog05_with_name_as_parent(capsys: CapSys):
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#method-decorator-with-arguments"""
+
+    def prog(name: str = "mario", age: int = 40):
+        return locals()
+
+    cmd = Command(prog)
+
+    @cmd.subcommand(name="subprog")
+    def subfunction(height: float):
+        return locals()
+
+    @cmd.subcommand(parent="subprog")
+    def internalsubfunction(city: str, state: str):
+        return locals()
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog05_main_with_name_as_parent in output
+
+    with pytest.raises(SystemExit) as e:
+        cmd.run(args=["subprog", "-h"])
+
+    assert e.value.code == 0
+    output = capsys.readouterr().out
+    assert helpmsg_prog05_subfunction_with_name_as_parent in output
+
+    assert cmd.run([]) == {"name": "mario", "age": 40}
+    assert cmd.run(["subprog", "1.70"]) == {"height": 1.70}
+    assert cmd.run(["subprog", "1.70", "internalsubfunction", "limeira", "SP"]) == {
+        "city": "limeira",
         "state": "SP",
     }
 
@@ -98,7 +340,7 @@ options:
 
 
 def test_prog06(capsys: CapSys):
-    from clig import command, subcommand, run
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#subcommands-using-function-decorators"""
 
     @command
     def main(verbose: bool = False):
@@ -151,7 +393,7 @@ options:
 
 
 def test_prog07(capsys: CapSys):
-    from clig import command, subcommand, run
+    """https://clig.readthedocs.io/en/latest/notebooks/subcommands.html#function-decorator-with-arguments"""
 
     @command
     def prog(name: str = "mario", age: int = 40):
