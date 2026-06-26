3b564aa84ed82652f4e69f3d73e6e9fa2f319eef
Diogo Rossi <rossi.diogo@gmail.com>
2026-06-26 10:32:17 -0300

    ✅ test: refactor test

--- tests/test_intg_11_12.py
+++ tests/test_intg_11_12.py
@@ -2,14 +2,14 @@ from clig import Arg, data, run
 
 
 def test_test_intg_11():
-    def main1(foo: Arg[list[int], data(action="append_const", const=666)] = [0]):
+    def main(foo: Arg[list[int], data(action="append_const", const=666)] = [0]):
         return locals()
 
-    assert run(main1, "--foo --foo".split()) == {"foo": [0, 666, 666]}
+    assert run(main, "--foo --foo".split()) == {"foo": [0, 666, 666]}
 
 
 def test_test_intg_12():
-    def main2(foo: Arg[list[int], data(action="extend")] = [0]):
+    def main(foo: Arg[list[int], data(action="extend")] = [0]):
         return locals()
 
-    assert run(main2, "--foo 23 --foo 45 89".split()) == {"foo": [0, 23, 45, 89]}
+    assert run(main, "--foo 23 --foo 45 89".split()) == {"foo": [0, 23, 45, 89]}
