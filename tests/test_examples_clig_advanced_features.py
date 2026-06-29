b6da308416214804328b072f7a585ee54b4d1bf5
Diogo Rossi <rossi.diogo@gmail.com>
2026-06-28 22:41:41 -0300

    ✅ test: change examples to be self contained

--- tests/test_examples_clig_advanced_features.py
+++ tests/test_examples_clig_advanced_features.py
@@ -1,21 +1,21 @@
 import pytest
 import clig
-from resources import CapSys
-
-help_msg_ex13 = """
-usage: main [-h] [-b BAR] foo
-
-positional arguments:
-  foo
-
-options:
-  -h, --help         show this help message and exit
-  -b BAR, --bar BAR
-""".strip()
+from resources import CapSys, deindent
 
 
 def test_ex13(capsys: CapSys):
     """https://clig.readthedocs.io/en/latest/notebooks/advancedfeatures.html#using-make-shorts"""
+
+    help_msg_ex13 = """
+    usage: main [-h] [-b BAR] foo
+    
+    positional arguments:
+      foo
+    
+    options:
+      -h, --help         show this help message and exit
+      -b BAR, --bar BAR
+    """
 
     def main(foo: str, bar: int = 42):
         return locals()
@@ -25,23 +25,22 @@ def test_ex13(capsys: CapSys):
 
     assert e.value.code == 0
     output = capsys.readouterr().out.strip()
-    assert output in help_msg_ex13
+    assert output == deindent(help_msg_ex13)
 
     assert clig.run(main, ["test", "--bar", "45"], make_shorts=True) == {"foo": "test", "bar": 45}
     assert clig.run(main, ["test", "-b", "13"], make_shorts=True) == {"foo": "test", "bar": 13}
 
 
-help_msg_ex14 = """
-usage: main [-h] -f FOO [-b BAR]
-
-options:
-  -h, --help         show this help message and exit
-  -f FOO, --foo FOO
-  -b BAR, --bar BAR
-""".strip()
-
-
 def test_ex14(capsys: CapSys):
+
+    help_msg_ex14 = """
+    usage: main [-h] -f FOO [-b BAR]    
+    
+    options:
+      -h, --help         show this help message and exit
+      -f FOO, --foo FOO
+      -b BAR, --bar BAR
+    """
 
     def main(foo: str, bar: int = 42):
         return locals()
@@ -51,7 +50,7 @@ def test_ex14(capsys: CapSys):
 
     assert e.value.code == 0
     output = capsys.readouterr().out.strip()
-    assert output in help_msg_ex14
+    assert output == deindent(help_msg_ex14)
 
     assert clig.run(main, ["--foo", "test", "--bar", "45"], make_flags=True, make_shorts=True) == {
         "foo": "test",
@@ -63,32 +62,32 @@ def test_ex14(capsys: CapSys):
     }
 
 
-help_msg_ex15 = """
-usage: main [-h] [-f FILE] [-F FILENAME] [-fi FILEDIR] [-FI FILEPATH]
-            [-fn FILE_NAME] [-fd FILE_DIR] [-fp FILE_PATH] [-fo FOLDER]
-            [-FO FOLDERNAME] [-fol FOLDERDIR] [-FOL FOLDERPATH]
-            [-fona FOLDER_NAME] [-fodi FOLDER_DIR] [-fopa FOLDER_PATH]
-
-options:
-  -h, --help            show this help message and exit
-  -f FILE, --file FILE
-  -F FILENAME, --filename FILENAME
-  -fi FILEDIR, --filedir FILEDIR
-  -FI FILEPATH, --filepath FILEPATH
-  -fn FILE_NAME, --file-name FILE_NAME
-  -fd FILE_DIR, --file-dir FILE_DIR
-  -fp FILE_PATH, --file-path FILE_PATH
-  -fo FOLDER, --folder FOLDER
-  -FO FOLDERNAME, --foldername FOLDERNAME
-  -fol FOLDERDIR, --folderdir FOLDERDIR
-  -FOL FOLDERPATH, --folderpath FOLDERPATH
-  -fona FOLDER_NAME, --folder-name FOLDER_NAME
-  -fodi FOLDER_DIR, --folder-dir FOLDER_DIR
-  -fopa FOLDER_PATH, --folder-path FOLDER_PATH
-""".strip()
-
-
 def test_ex15(capsys: CapSys):
+
+    help_msg_ex15 = """
+    usage: main [-h] [-f FILE] [-F FILENAME] [-fi FILEDIR] [-FI FILEPATH]
+                [-fn FILE_NAME] [-fd FILE_DIR] [-fp FILE_PATH] [-fo FOLDER]
+                [-FO FOLDERNAME] [-fol FOLDERDIR] [-FOL FOLDERPATH]
+                [-fona FOLDER_NAME] [-fodi FOLDER_DIR] [-fopa FOLDER_PATH]
+    
+    options:
+      -h, --help            show this help message and exit
+      -f FILE, --file FILE
+      -F FILENAME, --filename FILENAME
+      -fi FILEDIR, --filedir FILEDIR
+      -FI FILEPATH, --filepath FILEPATH
+      -fn FILE_NAME, --file-name FILE_NAME
+      -fd FILE_DIR, --file-dir FILE_DIR
+      -fp FILE_PATH, --file-path FILE_PATH
+      -fo FOLDER, --folder FOLDER
+      -FO FOLDERNAME, --foldername FOLDERNAME
+      -fol FOLDERDIR, --folderdir FOLDERDIR
+      -FOL FOLDERPATH, --folderpath FOLDERPATH
+      -fona FOLDER_NAME, --folder-name FOLDER_NAME
+      -fodi FOLDER_DIR, --folder-dir FOLDER_DIR
+      -fopa FOLDER_PATH, --folder-path FOLDER_PATH
+    """
+
     def main(
         file: str = ".",
         filename: str = ".",
@@ -111,4 +110,4 @@ def test_ex15(capsys: CapSys):
 
     assert e.value.code == 0
     output = capsys.readouterr().out.strip()
-    assert output in help_msg_ex15
+    assert output == deindent(help_msg_ex15)
--- tests/test_intg_00_randon.py
+++ tests/test_intg_00_randon.py
@@ -1,79 +1,98 @@
-from resources import CapSys
+from resources import CapSys, deindent
 from clig import Command
-
-helpmsg_default_section = """
-usage: main [-h] name age height {second} ...
-
-The main command
-
-positional arguments:
-  name        The name of the person
-  age         The age of the person
-  height      The height of the person
-  {second}
-    second    A function witout arguments
-
-options:
-  -h, --help  show this help message and exit
-
-This is my main command""".strip()
-
-helpmsg_subcommands_section = """
-usage: main [-h] name age height {second} ...
-
-The main command
-
-positional arguments:
-  name        The name of the person
-  age         The age of the person
-  height      The height of the person
-
-options:
-  -h, --help  show this help message and exit
-
-subcommands:
-  {second}
-    second    A function witout arguments
-
-This is my main command""".strip()
-
-
-@Command
-def main(name: str, age: int, height: float):
-    """The main command
-
-    This is my main command
-
-    Args:
-        name: The name of the person
-        age: The age of the person
-        height: The height of the person
-    """
-    return locals()
-
-
-def second():
-    """A function witout arguments
-
-    This functions runs without arguments
-    """
-    return locals()
 
 
 def test_intg_00_random(capsys: CapSys):
+
+    @Command
+    def main(name: str, age: int, height: float):
+        """The main command
+
+        This is my main command
+
+        Args:
+            name: The name of the person
+            age: The age of the person
+            height: The height of the person
+        """
+        return locals()
+
+    def second():
+        """A function witout arguments
+
+        This functions runs without arguments
+        """
+        return locals()
+
+    helpmsg_default_section = """
+    usage: main [-h] name age height {second} ...
+    
+    The main command
+    
+    positional arguments:
+      name        The name of the person
+      age         The age of the person
+      height      The height of the person
+      {second}
+        second    A function witout arguments
+    
+    options:
+      -h, --help  show this help message and exit
+    
+    This is my main command""".strip()
+
     main.new_subcommand(second)
     main.print_help()
-    output = capsys.readouterr().out
-    assert helpmsg_default_section in output
+    output = capsys.readouterr().out.strip()
+    assert deindent(helpmsg_default_section) == output
     assert main.run(["ghost", "10", "1.50"]) == {"name": "ghost", "age": 10, "height": 1.50}
     assert main.run(["ghost", "10", "1.50", "second"]) == {}
 
 
 def test_intg_00_random_subcommands_section(capsys: CapSys):
-    maincmd = Command(main.func, subcommands_title="subcommands")
+
+    def main(name: str, age: int, height: float):
+        """The main command
+
+        This is my main command
+
+        Args:
+            name: The name of the person
+            age: The age of the person
+            height: The height of the person
+        """
+        return locals()
+
+    def second():
+        """A function witout arguments
+
+        This functions runs without arguments
+        """
+        return locals()
+
+    helpmsg_subcommands_section = """
+    usage: main [-h] name age height {second} ...
+    
+    The main command
+    
+    positional arguments:
+      name        The name of the person
+      age         The age of the person
+      height      The height of the person
+    
+    options:
+      -h, --help  show this help message and exit
+    
+    subcommands:
+      {second}
+        second    A function witout arguments
+    
+    This is my main command""".strip()
+
+    maincmd = Command(main, subcommands_title="subcommands")
     maincmd.new_subcommand(second)
     maincmd.print_help()
-    output = capsys.readouterr().out
-    assert helpmsg_subcommands_section in output
+    output = capsys.readouterr().out.strip()
+    assert deindent(helpmsg_subcommands_section) == output
     assert maincmd.run(["ghost", "10", "1.50"]) == {"name": "ghost", "age": 10, "height": 1.50}
     assert maincmd.run(["ghost", "10", "1.50", "second"]) == {}
